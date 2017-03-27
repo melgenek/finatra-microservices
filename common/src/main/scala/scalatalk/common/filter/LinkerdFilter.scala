6acf15dc79923326caa77d927831afcd8b54d954
Yevhenii Melnyk <melnyk.yevhenii@gmail.com>
2017-03-27 22:19:57 +0300

    Linkerd log trace id from header

--- common/src/main/scala/scalatalk/common/filter/LinkerdFilter.scala
+++ common/src/main/scala/scalatalk/common/filter/LinkerdFilter.scala
@@ -1,6 +1,9 @@
 package scalatalk.common.filter
 
+import java.util.Base64
+
 import com.twitter.finagle.http.{Request, Response}
+import com.twitter.finagle.tracing.TraceId
 import com.twitter.finagle.{Service, SimpleFilter}
 import com.twitter.util.Future
 import org.slf4j.MDC
@@ -13,7 +16,9 @@ class LinkerdFilter extends SimpleFilter[Request, Response] {
 		val trace = request.headerMap.get("l5d-ctx-trace")
 		if (trace.isDefined) {
 			val lTrace = trace.get
-			MDC.put("lTrace", lTrace)
+			val traceBytes = Base64.getDecoder.decode(lTrace)
+			val traceId = TraceId.deserialize(traceBytes).map(_.traceId.toString()).getOrElse("errorTraceId")
+			MDC.put("lTrace", traceId)
 			Linkerd.storeTrace(lTrace) {
 				service(request)
 			}
